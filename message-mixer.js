d75ca3f45c4236f56427a73d11f0787af44c1724
Brian Walker <bwalkerwebdev@gmail.com>
2022-03-27 21:48:01 -0400

    Finish project

--- message-mixer.js
+++ message-mixer.js
@@ -1,66 +1,7 @@
 // Import the functions from encryptors.js here.
+let encryptors = require('./encryptors')
 
-// Encryption Functions
-/////////////////////////////////////////////
-
-const caesarCipher = (str, amount = 0) => {
-  if (amount < 0) {
-    return caesarCipher(str, amount + 26);
-  }
-  let output = '';
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (char.match(/[a-z]/i)) {
-      let code = str.charCodeAt(i);
-      if (code >= 65 && code <= 90) {
-        char = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-      } else if (code >= 97 && code <= 122) {
-        char = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-      }
-    }
-    output += char;
-  }
-  return output;
-};
-
-const symbolCipher = (str) => {
-  const symbols = {
-    'i': '!',
-    '!': 'i',
-    'l': '1',
-    '1': 'l',
-    's': '$',
-    '$': 's',
-    'o': '0',
-    '0': 'o',
-    'a': '@',
-    '@': 'a',
-    'e': '3',
-    '3': 'e',
-    'b': '6',
-    '6': 'b'
-  }
-
-  let output = '';
-  for (let i = 0; i < str.length; i++) {
-    let char = str.toLowerCase()[i];
-
-    if (symbols[char]) {
-      output += symbols[char]
-    } else {
-      output += char;
-    }
-  }
-  return output;
-}
-
-const reverseCipher = (sentence) => {
-  let words = sentence.split(' ');
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i].split('').reverse().join('');
-  }
-   return words.join(' ');
-};
+let { caesarCipher, symbolCipher, reverseCipher } = encryptors;
 
 // User Input / Output Logic
 /////////////////////////////////////////////
@@ -74,8 +15,8 @@ process.stdin.on('data', (userInput) => {
 the user chose when they ran the program. */
 function getEncryptionMethod() {
   let encryptionMethod;
-  
-  const encryptionType = process.argv[2];  
+
+  const encryptionType = process.argv[2];
   if (encryptionType === 'symbol') {
     encryptionMethod = symbolCipher;
   } else if (encryptionType === 'reverse') {
@@ -84,10 +25,10 @@ function getEncryptionMethod() {
     let amount = Number(process.argv[3]);
     if (Number.isNaN(amount)) {
       process.stdout.write(`Try again with a valid amount argument. \n`)
-      process.exit();  
+      process.exit();
     }
     encryptionMethod = (str) => caesarCipher(str, amount);
-  } 
+  }
   else {
     process.stdout.write(`Try again with a valid encryption type. \n`)
     process.exit();
@@ -99,7 +40,7 @@ function getEncryptionMethod() {
 
 /* Helper function for displaying the encrypted message to the user. */
 function displayEncryptedMessage(encryptionMethod, userInput) {
-  let str = userInput.toString().trim();    
+  let str = userInput.toString().trim();
   let output = encryptionMethod(str);
   process.stdout.write(`\nHere is your encrypted message:\n> ${output}\n`)
   process.exit();
--- super-encoder.js
+++ super-encoder.js
@@ -1,32 +1,41 @@
 // Import the encryptors functions here.
+let encryptors = require("./encryptors");
 
+let { caesarCipher, symbolCipher, reverseCipher } = encryptors;
 
 const encodeMessage = (str) => {
   // Use the encryptor functions here.
-  
-}
+  let encryptor1 = caesarCipher(str, amount = 13);
+  let encryptor2 = symbolCipher(encryptor1);
+  let encryptor3 = reverseCipher(encryptor2);
+  return encryptor3;
+};
 
 const decodeMessage = (str) => {
   // Use the encryptor functions here.
-  
-}
+  let decryptor1 = reverseCipher(str);
+  let decryptor2 = symbolCipher(decryptor1);
+  let decryptor3 = caesarCipher(decryptor2, amount = 13);
+  return decryptor3;
+
+};
 
 // User input / output.
 
 const handleInput = (userInput) => {
   const str = userInput.toString().trim();
   let output;
-  if (process.argv[2] === 'encode') {
+  if (process.argv[2] === "encode") {
     output = encodeMessage(str);
-  } 
-  if (process.argv[2] === 'decode') {
+  }
+  if (process.argv[2] === "decode") {
     output = decodeMessage(str);
-  } 
-  
-  process.stdout.write(output + '\n');
+  }
+
+  process.stdout.write(output + "\n");
   process.exit();
-}
+};
 
 // Run the program.
-process.stdout.write('Enter the message you would like to encrypt...\n> ');
-process.stdin.on('data', handleInput);
+process.stdout.write("Enter the message you would like to encrypt...\n> ");
+process.stdin.on("data", handleInput);
